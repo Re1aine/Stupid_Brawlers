8e36160acc4127f7ffa26536104ee4573e3c5c70
Re1aine <90120253+Re1aine@users.noreply.github.com>
2025-01-02 19:35:00 +0300

    Integrate Spine; Start working on spine player feature

--- Stupid Brawlers/Assets/Scripts/PlayerView.cs
+++ Stupid Brawlers/Assets/Scripts/PlayerView.cs
@@ -1,19 +1,29 @@
 using System;
+using Spine;
+using Spine.Unity;
 using UnityEngine;
 
 public class PlayerView : MonoBehaviour
 {
+    private const string TargetPoint = "aimt-target-IK";
     public Player Player => _player;
     public GunView GunView => _gunView;
     public PlayerInput Input => _input;
+
+    [SerializeField] private SkeletonAnimation _skeletonAnimation;
+    [SerializeField] private GameObject _shootPoint;
+    private IkConstraint _targetConstraint;
+    private Camera _camera;
+    private Bone _targetBone;
+    
     
     [SerializeField] private PlayerInput _input;
     [SerializeField] private GunView _gunView;
     [SerializeField] private AimHelper _aimHelpLine;
     [SerializeField] private Cursor _scope;
-    
+
     private Player _player;
-    
+
     public void Construct(Player player)
     {
         _player = player;
@@ -27,6 +37,33 @@ public class PlayerView : MonoBehaviour
     private void Awake()
     {
         TurnOffAim();
+        _camera = Camera.main;
+        _targetConstraint = _skeletonAnimation.Skeleton.IkConstraints.Find(c => c.ToString() == TargetPoint);
+        _targetBone = _targetConstraint.Target;
+        
+    }
+
+    private void Update()
+    {
+        Vector3 mousePosition = new Vector3(
+            _camera.ScreenToWorldPoint(UnityEngine.Input.mousePosition).x,
+            _camera.ScreenToWorldPoint(UnityEngine.Input.mousePosition).y,
+            0 );
+        
+        Vector3 direction = (mousePosition - _shootPoint.transform.position).normalized;
+        
+//        Vector2 originalPosition = new Vector2(_targetBone.X, _targetBone.Y);
+//
+//// Переводим мировую позицию мыши в локальные координаты
+//        var skeletonSpacePoint = _skeletonAnimation.transform.InverseTransformPoint(mousePosition);
+//
+//// Сохраняем относительное смещение
+//        skeletonSpacePoint.x += originalPosition.x;
+//        skeletonSpacePoint.y += originalPosition.y;
+//
+//// Устанавливаем позицию кости
+//        _targetBone.SetLocalPosition(skeletonSpacePoint);
+        
     }
 
     private void SetAimMode(AimMode mode)
